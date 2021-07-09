766221581c5f840fb361032b9f8f8fa90d9c73b6
patrykbura <patrykbura@gmail.com>
2021-07-09 19:59:44 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <DataTable msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import DataTable from './components/DataTable/DataTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DataTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DataTable/DataTable.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    Hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+  @import 'DataTable.scss';
+</style>
