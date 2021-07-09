befb531911744d868191f760ce8c5073624992cf
patrykbura <patrykbura@gmail.com>
2021-07-09 20:28:01 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <DataTable msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import DataTable from './components/DataTable/DataTable.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    DataTable
+    HelloWorld
   }
 }
 </script>
